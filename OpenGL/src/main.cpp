89902cfece6eebe17cc17a14b76f1c23c986b12e
Alex Brittain <alexbrittain9@gmail.com>
2022-07-21 21:20:59 -0500

    Clear color test

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -86,29 +86,49 @@ int main(void)
 
 		Renderer renderer;
 
-		test::TestClearColor test;
+		test::Test* currentTest = nullptr;
+		test::TestMenu* testMenu = new test::TestMenu(currentTest);
+		currentTest = testMenu;
+
+		testMenu->RegisterTest<test::TestClearColor>("Clear Color");
+
 
 		while (!glfwWindowShouldClose(window))
 		{
+			GLCALL(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
 			renderer.Clear();
-
-			test.OnUpdate(0.0f);
-			test.OnRender();
 
 			ImGui_ImplOpenGL3_NewFrame();
 			ImGui_ImplGlfw_NewFrame();
 			ImGui::NewFrame();
-			test.OnImGuiRender();
+
+			if(currentTest)
+			{
+				currentTest->OnUpdate(0.0f);
+				currentTest->OnRender();
+				ImGui::Begin("Test");
+				if(currentTest != testMenu && ImGui::Button("<-"))
+				{
+					delete currentTest;
+					currentTest = testMenu;
+				}
+				currentTest->OnImGuiRender();
+				ImGui::End();
+			}
+
 
 			ImGui::Render();
 			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 			glfwSwapBuffers(window);
-
 			glfwPollEvents();
 		}
-
+		delete currentTest;
+		if (currentTest != testMenu)
+			delete testMenu;
 	}
+
+
 	ImGui_ImplOpenGL3_Shutdown();
 	ImGui_ImplGlfw_Shutdown();
 	ImGui::DestroyContext();
--- OpenGL/src/tests/Test.h
+++ OpenGL/src/tests/Test.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <functional>
+#include <iostream>
+#include <vector>
+
+
 namespace test
 {
 	class Test
@@ -12,4 +17,25 @@ namespace test
 		virtual void OnRender() {}
 		virtual void OnImGuiRender() {}
 	};
+
+	class TestMenu :public Test
+	{
+	public:
+		TestMenu(Test*& currentTestPointer);
+
+		void OnImGuiRender() override;
+
+		template <typename T>
+		void RegisterTest(const std::string& name)
+		{
+			std::cout << "Registering test " << name << std::endl;
+			m_Tests.push_back(std::make_pair(name, []() {return new T(); })); // lamda
+		}
+	private:
+		// Test menu can write to Test* and change current test
+		Test*& m_currentTest;
+		// Vector of pairs with string and function returning a test* so we can construct and destruct as needed
+		std::vector < std::pair<std::string, std::function<Test* ()>>> m_Tests;
+	};
 }
+
